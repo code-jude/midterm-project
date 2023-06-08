30c872df71104bd01a53285b3fd86758f4fa5afb
code-jude <goodonone@outlook.com>
2023-06-08 08:42:37 -0700

    Added Cover Image

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,9 +7,9 @@ import { NotfoundComponent } from './notfound/notfound.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "/home", pathMatch: "full" },
-  { path: "/home", component: HomeComponent },
-  { path: "/about", component: AboutComponent },
-  { path: "/jobs/:id", component: JobsComponent },
+  { path: "home", component: HomeComponent },
+  { path: "about", component: AboutComponent },
+  { path: "jobs/:id", component: JobsComponent },
   { path: "**", component: NotfoundComponent }
 ];
 
